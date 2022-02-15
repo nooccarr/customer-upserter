f277fad4b48b03a17a1ba6212476b445636f9c59
Soohyun Lim <d2axasap@gmail.com>
2022-02-14 20:48:04 -0500

    Create sample data folder with configuration and data file. Add dependencies to package.json. Multer is for uploading multiple files and customerio-node is for using customerio.io APIs. Have put request retrieve configuration and data file from the client and add new user to customer.io using their API

--- server/index.js
+++ server/index.js
@@ -1,15 +1,56 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const multer = require('multer');
+const axios = require('axios');
+
 const app = express();
-const PORT = 3000;
+const upload = multer();
+
+const { siteId, apiKey } = require('./config.js');
+
+const { TrackClient, RegionUS } = require("customerio-node");
+let cio = new TrackClient(siteId, apiKey, { region: RegionUS });
 
 app.use(express.json());
+app.use(upload.array('inputs'));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.put('/', (req, res) => {
+  let configuration = req.files[0].buffer;
+  let data = req.files[1].buffer;
+  configuration = JSON.parse(configuration.toString());
+  data = JSON.parse(data.toString());
+  configuration[siteId] = apiKey;
+  // console.log(configuration);
+  // console.log(data[0]);
+  // return axios.put(`https://track.customer.io/api/v1/customers/${identifier}`, req.body)
+  cio.identify(data[0].id, data[0])
+    .then(results => {
+  //     console.log(Object.keys(results))
+      res.send(results);
+    })
+    .catch(err => console.log(err.response.status));
 })
+
 app.get('*', (req, res) => {
   res.send('404 Page Not Found');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+
+
+// to create people in a workspace with default settings, the id (5) can also be an email address.
+// when creating people using an email address, do not include an email attribute.
+// cio.identify(5, {
+//   email: 'customer@example.com',
+//   created_at: 1361205308,
+//   first_name: 'Bob',
+//   plan: 'basic'
+// });
