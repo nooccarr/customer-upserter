914567e20b1cc62fc84e5318bd170d40b60a5c83
Soohyun Lim <d2axasap@gmail.com>
2022-02-15 14:48:54 -0500

    Remove axios dependency. Use promise all to make upsert API request to customer.io concurrently

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const multer = require('multer');
-const axios = require('axios');
 
 const app = express();
 const upload = multer();
 
 const { siteId, apiKey } = require('./config.js');
-
 const { TrackClient, RegionUS } = require("customerio-node");
-let cio = new TrackClient(siteId, apiKey, { region: RegionUS });
 
 app.use(express.json());
 app.use(upload.array('inputs'));
@@ -18,21 +15,27 @@ app.get('/', (req, res) => {
 });
 
 app.put('/', (req, res) => {
-  let configuration = req.files[0].buffer;
+  let config = req.files[0].buffer;
+  config = JSON.parse(config.toString());
+  config.siteId = siteId;
+  config.apiKey = apiKey;
+
   let data = req.files[1].buffer;
-  configuration = JSON.parse(configuration.toString());
   data = JSON.parse(data.toString());
-  configuration[siteId] = apiKey;
-  // console.log(configuration);
-  // console.log(data[0]);
-  // return axios.put(`https://track.customer.io/api/v1/customers/${identifier}`, req.body)
-  cio.identify(data[0].id, data[0])
-    .then(results => {
-  //     console.log(Object.keys(results))
-      res.send(results);
-    })
-    .catch(err => console.log(err.response.status));
-})
+  // TODO:
+  // (v) implement concurrent request: axios all
+  // ( ) integrate parallelism, userId, and mappings functionality
+
+  Promise.all(data.map((customer) => {
+    let cio = new TrackClient(config.siteId, config.apiKey, { region: RegionUS });
+    cio.identify(customer.id, customer)
+      // .then(res => console.log(res))
+      // .catch(err => res.sendStatus(401));
+  }))
+    .then(results => res.sendStatus(200))
+    .catch(err => res.sendStatus(401));
+
+});
 
 app.get('*', (req, res) => {
   res.send('404 Page Not Found');
@@ -41,16 +44,15 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+// TEST CASES:
+// curl --request PUT \
+// --url https://track.customer.io/api/v1/customers/person@example.com \
+// --header "Authorization: Basic $(echo -n site_id:api_key | base64)" \
+// --header 'content-type: application/json' \
+// --data '{"id":"id1234"}'
 
-
-
-
-
-// to create people in a workspace with default settings, the id (5) can also be an email address.
-// when creating people using an email address, do not include an email attribute.
-// cio.identify(5, {
-//   email: 'customer@example.com',
-//   created_at: 1361205308,
-//   first_name: 'Bob',
-//   plan: 'basic'
-// });
+// curl --request PUT \
+// --url https://track.customer.io/api/v1/customers/id1234 \
+// --header "Authorization: Basic $(echo -n site_id:api_key | base64)" \
+// --header 'content-type: application/json' \
+// --data '{"email":"person@example.com"}'
