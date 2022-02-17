ef9409f98b1bd1ed79d8009eb235887357be8775
Soohyun Lim <d2axasap@gmail.com>
2022-02-17 00:36:06 -0500

    Add axios dependency. Create app.get('/customers/) for looking up customer's attribute. This can be used to check if customer is already in Customer.io. Add product-question.md.

---
+++ product-question.md
@@ -0,0 +1,4 @@
+Suppose we've released this script to a customer and they're happy with the upsert behavior. However, another customer already has many users uploaded to Customer.io and would rather not have Hightouch insert users. Instead, they only want to update attributes on existing users.
+
+Would the Customer.io API allow this customer use case to be supported? If you arrive at a solution, how hacky would you rate it on a scale of 1 to 10? Are there are any edge cases you would be concerned about? If you think a solution doesn't exist, give some reasons why you think it's not possible.
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const multer = require('multer');
+const axios = require('axios');
 
 const app = express();
 const upload = multer();
 
+const { appApiKey } = require('./config.js');
 const { TrackClient, RegionUS } = require("customerio-node");
 
 app.use(express.json());
@@ -62,6 +64,19 @@ app.put('/', async (req, res) => {
   }
 
   return res.sendStatus(200);
+});
+
+app.get('/customers', (req, res) => {
+  let id = req.query.id;
+  return axios.get(
+      `https://beta-api.customer.io/v1/api/customers/${id}/attributes`,
+      {
+        params: { id_type: 'id' },
+        headers: { Authorization: `Bearer ${appApiKey}` }
+      }
+    )
+    .then(result => res.send(result.data))
+    .catch(err => res.sendStatus(404));
 });
 
 app.get('*', (req, res) => {
